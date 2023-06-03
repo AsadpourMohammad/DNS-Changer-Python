6059aba26df10ed8a97c0780a063ff39f10e011d
Mohammad Asadpour <For.Mohammad.Asadpour@gmail.com>
2023-06-03 12:43:50 +0330

    Code changed to get the dns servers for the active Network Connection.

--- main.py
+++ main.py
@@ -1,14 +1,15 @@
 import subprocess
+import re
 
 
-def get_active_interface():
-    output = subprocess.check_output("ipconfig", shell=True).decode('utf-8')
+def get_dns_servers():
+    output = subprocess.check_output("ipconfig /all", shell=True, text=True)
 
-    active_interfaces = []
     current_interface = ""
     media_disconnected = False
+    dns_servers = {}
 
-    for line in output.split('\n'):
+    for line in output.splitlines():
         if "adapter" in line:
             current_interface = line.strip().rstrip(':')
             media_disconnected = False
@@ -16,10 +17,20 @@ def get_active_interface():
                 current_interface = ""
                 continue
         elif "Media State" in line:
-            if "Media disconnected" in line:
-                media_disconnected = True
+            media_disconnected = "Media disconnected" in line
         elif "IPv4 Address" in line and not media_disconnected and current_interface:
-            active_interfaces.append(current_interface)
+            dns_servers[current_interface] = None
+        elif "DNS Servers" in line:
+            dns_ips = re.findall(r'\d+\.\d+\.\d+\.\d+', line)
+            if dns_ips:
+                dns_servers[current_interface] = dns_ips
+        elif dns_servers.get(current_interface) is not None:
+            additional_dns_ips = re.findall(r'\d+\.\d+\.\d+\.\d+', line)
+            if additional_dns_ips:
+                dns_servers[current_interface].extend(additional_dns_ips)
 
-    return active_interfaces
+    return dns_servers
 
+
+if __name__ == '__main__':
+    print(get_dns_servers())
