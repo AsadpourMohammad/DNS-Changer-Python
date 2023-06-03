13c3a7cbe1e4301716321aef73508779def67e3f
Mohammad Asadpour <For.Mohammad.Asadpour@gmail.com>
2023-06-03 12:16:39 +0330

    Function Added to get active Network Connections.

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
-if __name__ == '__main__':
-    print("We're still here.")
-    
+import subprocess
+
+
+def get_active_interface():
+    output = subprocess.check_output("ipconfig", shell=True).decode('utf-8')
+
+    active_interfaces = []
+    current_interface = ""
+    media_disconnected = False
+
+    for line in output.split('\n'):
+        if "adapter" in line:
+            current_interface = line.strip().rstrip(':')
+            media_disconnected = False
+            if "(WSL)" in current_interface:
+                current_interface = ""
+                continue
+        elif "Media State" in line:
+            if "Media disconnected" in line:
+                media_disconnected = True
+        elif "IPv4 Address" in line and not media_disconnected and current_interface:
+            active_interfaces.append(current_interface)
+
+    return active_interfaces
+
