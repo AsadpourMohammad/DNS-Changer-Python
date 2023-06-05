cd158fb6ef8b0164d48c4d37b1b7189176c356f7
Mohammad Asadpour <For.Mohammad.Asadpour@gmail.com>
2023-06-06 00:34:41 +0330

    Minor changes.

--- dns_changer_cli/dns_provider.py
+++ dns_changer_cli/dns_provider.py
@@ -1,11 +1,8 @@
 import json
 
-from rich.console import Console
-from rich.panel import Panel
+from dns_changer_cli.wrappers.rich_wrappers import TextPanelWrapper, print_panel, print_text
 
 from dns_utils.dns_windows_utils import get_all_networks_and_dns_servers, is_network_dns_auto
-
-__console__ = Console()
 
 __DNS_PROVIDERS__ = None
 
@@ -37,16 +34,7 @@ def get_provider_of_servers(servers: tuple[str]) -> str:
 
 
 def __read_dns_providers_from_json__() -> dict[str, tuple[str]]:
-    try:
-        with open('dnsProviders.json', 'r') as dns_providers_file:
-            saved_dns_providers = json.load(dns_providers_file)
-
-        for provider in saved_dns_providers:
-            saved_dns_providers[provider] = tuple(saved_dns_providers[provider])
-
-        return saved_dns_providers
-    except (FileNotFoundError, json.JSONDecodeError):
-        json_err_msg = """
+    json_err_msg = """
         An error occurred during the loading of DNS Providers JSON file.
         
         Please create a dnsProviders.json file in the same directory as the current script with
@@ -60,8 +48,17 @@ def __read_dns_providers_from_json__() -> dict[str, tuple[str]]:
         
         You can use the app now, but you will have to enter the DNS servers manually.
         """
+    
+    try:
+        with open('dnsProviders.json', 'r') as dns_providers_file:
+            saved_dns_providers = json.load(dns_providers_file)
 
-        panel = Panel(json_err_msg, title="JSON READING UNSUCCESSFUL", style="bold magenta", width=112)
-        __console__.print(panel)
+        for provider in saved_dns_providers:
+            saved_dns_providers[provider] = tuple(saved_dns_providers[provider])
+
+        return saved_dns_providers
+    except (FileNotFoundError, json.JSONDecodeError):
+        panel_wrapper = TextPanelWrapper(title="JSON READING UNSUCCESSFUL", text=json_err_msg)
+        print_panel(panel_wrapper.panel)
 
         return {}
